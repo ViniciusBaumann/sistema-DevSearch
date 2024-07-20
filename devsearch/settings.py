bae1e2c4bbd63830b6bf47c5be9e35ec37645929
vinni <viniciusbaumann@gmail.com>
2024-07-20 13:38:38 -0300

    creation of .env

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!b8+%xavn(1hde^)1%7m*86ji948v2l9ls&b&he1-l3g(o3rp1'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
