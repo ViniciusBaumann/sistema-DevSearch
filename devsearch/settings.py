c279badadcba9e6cc869fccfcac7256dd1c18057
vinni <viniciusbaumann@gmail.com>
2024-07-26 13:24:21 -0300

    add: sending emails

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import os
 from decouple import config
+from dotenv import load_dotenv
+# Load environment variables from .env file
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -116,6 +119,15 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+
+#Backend - Envio de Email
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'viniciusbaumann@gmail.com'
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASSWORD')
 
 
 # Static files (CSS, JavaScript, Images)
--- users/signals.py
+++ users/signals.py
@@ -2,6 +2,9 @@ from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 from .models import Profile
+from django.core.mail import send_mail
+from django.conf import settings
+from django.core.exceptions import ObjectDoesNotExist
 
 #@receiver(post_delete, sender=Profile)
 def createProfile(sender, instance, created, **kwargs):
@@ -14,6 +17,17 @@ def createProfile(sender, instance, created, **kwargs):
             name=user.first_name
         )
         
+        subject = "Welcome to DevSearch"
+        message = "We glad you are here!"
+        
+        send_mail(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,
+            [profile.email],
+            fail_silently=False,
+        )
+        
 def updateUser(sender, instance, created, **kwargs):
     profile = instance
     user = profile.user
@@ -24,8 +38,14 @@ def updateUser(sender, instance, created, **kwargs):
         user.save()
         
 def deleteUser(sender, instance, **kwargs):
-    user = instance.user
-    user.delete()
+    try:
+        user = instance.user
+    except ObjectDoesNotExist:
+        user = None
+    
+    if user:
+        # Perform your delete actions here
+        pass
 
 
 post_save.connect(createProfile, sender=User)
